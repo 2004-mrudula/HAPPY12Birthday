70df1acf07648cdc43188b1e3a48c5b3195959ae
2004-mrudula <mrudula_temkar_ece@moderncoe.edu.in>
2025-05-13 10:25:38 -0700

    Update effect.js

--- effect.js
+++ effect.js
@@ -174,7 +174,7 @@ $('document').ready(function(){
 
 		function msgLoop(i) {
     $("p:nth-child(" + i + ")").fadeIn('slow').delay(4000).promise().done(function () {
-        $("p:nth-child(" + i + ")").fadeOut('fast', function() { 
+        $("p:nth-child(" + i + ")").fadeOut('fast', function() { // Fade out the current message *before* showing the next
             i = i + 1;
             if (i <= 50) {
                 msgLoop(i); // Call msgLoop to display the next message
@@ -186,6 +186,7 @@ $('document').ready(function(){
         });
     });
 }
+
 
 
 			else{
